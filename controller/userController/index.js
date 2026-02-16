08056dbcb8d3fc94fc4f5d679ba7e1c519a9e135
SoraK93 <SoraK93@users.noreply.github.com>
2026-02-16 10:58:49 +0530

    updating user controller index file keeping it in sync with all the recent changes

--- controller/userController/index.js
+++ controller/userController/index.js
@@ -1,12 +1,16 @@
+const bcrypt = require("bcrypt")
+
 const pool = require("../../model/database");
 const queryReturnError = require("../../utility/api-errors");
-const genPatchRouteQueryList = require("../../utility/api-helper");
+const { genPatchRouteQueryList } = require("../../utility/api-helper");
 
-const dependencies = { pool, queryReturnError, genPatchRouteQueryList };
+const dependencies = { bcrypt, pool, queryReturnError, genPatchRouteQueryList };
 
 module.exports = {
   getUsers: require("./_getUsers")(dependencies),
-  getUserById: require("./_getUserById")(dependencies),
+  getSellerById: require("./_getSellerById")(dependencies),
   updateUserById: require("./_updateUserById")(dependencies),
   deleteUserById: require("./_deleteUserById")(dependencies),
+  getUserEmail: require("./_getUserEmail")(dependencies),
+  changeUserPassword: require("./_changeUserPassword")(dependencies),
 };
