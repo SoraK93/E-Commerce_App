35bbc42f357e60b0e947904d455e3b8d980b209d
SoraK93 <SoraK93@users.noreply.github.com>
2026-02-16 11:04:50 +0530

    change header if there is a active session

--- views/src/features/Header/Header.jsx
+++ views/src/features/Header/Header.jsx
@@ -1,16 +1,40 @@
-import { Link } from "react-router"
+import { Link, useSubmit } from "react-router";
+import { useSelector } from "react-redux";
+import { selectUser } from "../users/usersSlice";
 
 const Header = () => {
+  const userInfo = useSelector(selectUser.info);
+  const submit = useSubmit();
+
+  const handleClick = () => {
+    submit(null, { method: "POST", action: "/auth/logout" });
+  };
+
   return (
     <header>
       <nav>
         <ul>
-          <li>LOGO</li>
+          <li>
+            <Link to="/">LOGO</Link>
+          </li>
           <li>
             <input type="text" placeholder="Search Bar" />
           </li>
           <li>cart icon</li>
-          <li><Link to="/auth/login">Login</Link></li>
+          {userInfo.name ? (
+            <>
+              <li>
+                <Link to={`/user`}>{userInfo.name}</Link>
+              </li>
+              <li>
+                <a onClick={handleClick}>Logout</a>
+              </li>
+            </>
+          ) : (
+            <li>
+              <Link to="/auth/login">Login</Link>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
