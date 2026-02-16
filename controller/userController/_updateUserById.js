236ec3b14221949734d72af793748bd836a707e4
SoraK93 <SoraK93@users.noreply.github.com>
2026-02-16 10:53:30 +0530

    after user update return updated user data keeps our frontend render and db in sync

--- controller/userController/_updateUserById.js
+++ controller/userController/_updateUserById.js
@@ -1,14 +1,16 @@
 module.exports =
   ({ pool, genPatchRouteQueryList }) =>
   async (req, res, next) => {
-    const userId = req.params.userId;
+    const userId = req.user.id;
 
     const [updateFields, updateValues] = genPatchRouteQueryList(req.body, next);
 
     const result = await pool.query(
-      `UPDATE customers_details SET ${updateFields.join(", ")} WHERE id = $${updateValues.length + 1}`,
+      `UPDATE customers_details SET ${updateFields.join(", ")} WHERE id = $${updateValues.length + 1} RETURNING name, phone, address, is_seller`,
       [...updateValues, userId],
     );
 
-    res.status(204).end();
+    res
+      .status(200)
+      .json({ message: "Update successful", user: result.rows[0] });
   };
