330e2df38aff9717b05d71cdf2f9b63c4cea9bac
SoraK93 <SoraK93@users.noreply.github.com>
2026-02-17 13:53:34 +0530

    add seller related request routes and functionality

--- router/users.js
+++ router/users.js
@@ -1,26 +1,28 @@
 const express = require("express");
 const {
   getUsers,
-  getSellerById,
   updateUserById,
   deleteUserById,
   getUserEmail,
   changeUserPassword,
 } = require("../controller/userController");
+const { getSellerById, getProductBySellerId } = require("../controller/sellerController");
 const { checkOwnerShip, checkLoggedIn } = require("../utility/api-auth");
 
 const user = express.Router();
 
 user.get("/", checkLoggedIn, getUsers);
 
-user.get("/seller-profile", getSellerById);
-
 user.patch("/update", checkLoggedIn, updateUserById);
 
 user.delete("/delete", checkLoggedIn, deleteUserById);
 
-user.get("/email", checkLoggedIn, getUserEmail)
+user.get("/email", checkLoggedIn, getUserEmail);
 
-user.patch("/change-password", checkLoggedIn, changeUserPassword)
+user.patch("/change-password", checkLoggedIn, changeUserPassword);
+
+user.get("/seller-profile", getSellerById);
+
+user.get("/view-product", checkLoggedIn, getProductBySellerId);
 
 module.exports = user;
