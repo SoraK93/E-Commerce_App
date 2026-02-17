fb9e499c4a15e8b6c77f125cfa2d9d4d9f96ad15
SoraK93 <SoraK93@users.noreply.github.com>
2026-02-17 13:57:22 +0530

    user can add products now

---
+++ views/src/features/users/seller/AddProduct.jsx
@@ -0,0 +1,132 @@
+import { useEffect, useState } from "react";
+import { handleChange } from "../../utilities/event-helper";
+import { Link, useLocation, useNavigate } from "react-router";
+import { addProductBySeller, editProductBySeller } from "./sellerAPI";
+
+const AddProduct = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const product = location.state?.product;
+  const isEdit = location.pathname.includes("edit");
+
+  const formStruct = {
+    name: "",
+    description: "",
+    in_stock: "",
+    price: "",
+  };
+
+  const [newProduct, setNewProduct] = useState(null);
+  const [formData, setFormData] = useState(formStruct);
+
+  const heading = <h2>{isEdit ? "Edit product" : "Add new Product"}</h2>;
+  const submitButton = (
+    <button>{isEdit ? "Update Product" : "Add Product"}</button>
+  );
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const data = isEdit
+        ? await editProductBySeller({})
+        : await addProductBySeller(formData);
+      setNewProduct(data);
+    } catch (err) {
+      console.error("Error adding product:", err);
+      alert(`Failed to add product: ${err.message}`);
+    }
+  };
+
+  useEffect(() => {
+    if (isEdit && product) {
+      setFormData({
+        name: product.product_name,
+        description: product.description,
+        in_stock: product.in_stock,
+        price: product.price,
+      });
+    } else {
+      setFormData(formStruct);
+    }
+  }, [isEdit, product]);
+
+  return (
+    <>
+      {newProduct && (
+        <div>
+          <p>Status: {newProduct.message}</p>
+          <Link to={`/product/${newProduct.id}`}>{newProduct.name}</Link>
+        </div>
+      )}
+      <div>
+        {heading}
+        <form method="post" onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <br />
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={formData.name}
+              onChange={(e) =>
+                handleChange(setFormData, "name", e.target.value)
+              }
+              required
+              autoComplete=""
+            />
+          </div>
+          <div>
+            <label htmlFor="description">Description:</label>
+            <br />
+            <input
+              type="text"
+              name="description"
+              id="description"
+              value={formData.description}
+              onChange={(e) =>
+                handleChange(setFormData, "description", e.target.value)
+              }
+              required
+              autoComplete=""
+            />
+          </div>
+          <div>
+            <label htmlFor="stock">Stock (in pcs):</label>
+            <br />
+            <input
+              type="number"
+              name="stock"
+              id="stock"
+              value={formData.in_stock}
+              onChange={(e) =>
+                handleChange(setFormData, "in_stock", e.target.value)
+              }
+              required
+              autoComplete=""
+            />
+          </div>
+          <div>
+            <label htmlFor="price">Price:</label>
+            <br />
+            <input
+              type="number"
+              name="price"
+              id="price"
+              value={formData.price}
+              onChange={(e) =>
+                handleChange(setFormData, "price", e.target.value)
+              }
+              required
+              autoComplete=""
+            />
+          </div>
+          {submitButton}
+        </form>
+      </div>
+    </>
+  );
+};
+
+export { AddProduct };
