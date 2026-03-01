163bb3d6e6ea1bb51f0c5a90fb82bdd13f4cd47b
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2026-03-01 20:22:00 +0530

    update store, add types and a hooks file adding state types to redux dispatch and selector

--- views/src/app/store.ts
+++ views/src/app/store.ts
@@ -2,11 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import { users, products } from "../features";
 import cartReducer from "../features/cart/api/cartSlice";
+import orderReducer from "../features/orders/api/orderSlice";
 
 export const store = configureStore({
   reducer: {
     products: products.reducer,
     users: users.reducer,
     cart: cartReducer,
+    order: orderReducer,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
