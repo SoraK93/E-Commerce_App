64ba63fb57965c7e92ac9deacf35630748d9b30d
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2026-03-18 12:37:10 +0530

    add product route to api path config

---
+++ Backend/controller/products.py
@@ -0,0 +1,18 @@
+def fetch_all_product():
+    return {"message": "product works"}
+
+
+def fetch_product_by_id():
+    return {"message": "Should return a specific product"}
+
+
+def make_a_new_product():
+    return {"message": "Should create a new product"}
+
+
+def edit_existing_product_detail():
+    return {"message": "should update a single product"}
+
+
+def delete_existing_product():
+    return {"message": "should delete a specific product"}
--- Backend/router/products.py
+++ Backend/router/products.py
@@ -1,11 +1,33 @@
 from fastapi import APIRouter
 
-products = APIRouter(
-  prefix="/products",
-  tags=["products"]
+import Backend.controller.products as products
+
+router = APIRouter(
+    prefix="/products",
+    tags=["products"]
 )
 
 
-@products.get("/")
-async def prod():
-    return {"message": "product works"}
+@router.get("/")
+async def get_products():
+    return products.fetch_all_product()
+
+
+@router.get("/{product_id}")
+async def get_product_by_id():
+    return products.fetch_product_by_id()
+
+
+@router.post("/")
+async def create_new_product():
+    return products.make_a_new_product()
+
+
+@router.patch("/{product_id}")
+async def update_product_by_id():
+    return products.edit_existing_product_detail()
+
+
+@router.delete("/{product_id}")
+async def delete_product_by_id():
+    return products.delete_existing_product()
