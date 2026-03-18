8bacaee41245b030f26531565b710172ca5a437d
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2026-03-18 12:34:46 +0530

    frontend now supports https

--- views/vite.config.ts
+++ views/vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), mkcert()],
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx']
-  }
-})
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  server: {
+    https: {},
+  },
+});
