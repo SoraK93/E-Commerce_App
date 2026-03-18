fb12798e8e3df8d97c325600f105c2a956286cca
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2026-03-18 12:36:13 +0530

    Add session, cors, and https support to backend

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,14 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from starlette.middleware.sessions import SessionMiddleware
+import uvicorn
 
-from .router.products import products
+
+from Backend.router.products import router as products
+
+load_dotenv()
 
 app = FastAPI(
     title="E-Commerce App Backend",
@@ -8,9 +16,33 @@ app = FastAPI(
     version="0.0.1"
 )
 
+origins = ["http://localhost:5173"]
+
+app.add_middleware(SessionMiddleware,
+                   secret_key=os.environ["S_SECRET"],
+                   max_age=60*60*24,
+                   same_site="lax")
+
+app.add_middleware(CORSMiddleware,
+                   allow_origins=origins,
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"])
+
 app.include_router(products)
 
 
 @app.get("/")
 async def root():
-    return { "message": "Welcome to E-Commerce App Backend"}
+    return {"message": "Welcome to E-Commerce App Backend"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "Backend.main:app",
+        host="0.0.0.0",
+        port=int(os.environ["SERVER_PORT"]),
+        reload=True,
+        ssl_certfile="./localhost+3.pem",
+        ssl_keyfile="./localhost+3-key.pem"
+    )
