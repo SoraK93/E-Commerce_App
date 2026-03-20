a5f3c5aa2883561d029b864c8555aabc6553fafb
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2026-03-20 19:34:38 +0530

    add session model

---
+++ Backend/model/session_model.py
@@ -0,0 +1,24 @@
+import uuid
+from datetime import datetime, timedelta, timezone
+from typing import Literal
+
+from pydantic import EmailStr
+from sqlalchemy import Column, DateTime
+from sqlmodel import SQLModel, Field, TEXT
+
+user_role = Literal["admin", "seller", "customer"]
+
+
+class SessionModel(SQLModel, table=True):
+    __tablename__ = "session_id"
+
+    id: uuid.UUID = Field(default_factory=uuid.uuid4,
+                          index=True, primary_key=True)
+
+    role: user_role = Field(
+        default="customer", nullable=False)
+
+    email: EmailStr = Field(sa_type=TEXT, nullable=False)
+
+    expire_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc) + timedelta(hours=24),
+                                sa_column=Column(DateTime(timezone=True)))
