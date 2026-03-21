52a77cf864df83b7b1be4db04817cf0109432be4
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2026-03-21 10:55:14 +0530

    Search db to find stored user info

--- Backend/controller/auth.py
+++ Backend/controller/auth.py
@@ -1,8 +1,18 @@
+from fastapi import HTTPException
+
+from model.user_model import UserModel
 from model.database import SessionDep
+from sqlmodel import select
 
 
 def initiate_login(email: str, password: str, session: SessionDep):
-    print(email, password)
+    user_in_db = session.exec(select(UserModel).where(UserModel.email == email)).first()
+    if not user_in_db:
+        raise HTTPException(status_code=404, detail="Incorrect Email or Password. Please try again.")
+    # TODO: create password hashing. currently using simple implementation
+    if user_in_db.password != password:
+        raise HTTPException(status_code=401, detail="Incorrect Email or Password. Please try again.")
+
     return {"message": "Login process starts"}
 
 
