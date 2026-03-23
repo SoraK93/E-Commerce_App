2cd63370f3cdcb8f52847606f4fb6cb3d354fefd
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2026-03-23 09:39:08 +0530

    Changing database driver from psycopy2-binary to asyncpg Now all our database calls will be non-blocking.

--- Backend/model/database.py
+++ Backend/model/database.py
@@ -1,21 +1,23 @@
 import os
+from typing import Annotated, AsyncGenerator
+
 from dotenv import load_dotenv
 from fastapi import Depends
-from sqlmodel import create_engine, Session
-from typing import Annotated, Generator
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlmodel.ext.asyncio.session import AsyncSession
 
 load_dotenv()
 
 DATABASE_URL = os.environ["DB_URL"]
 
-engine = create_engine(url=DATABASE_URL,
-                       echo=True,
-                       pool_pre_ping=True)
+engine = create_async_engine(url=DATABASE_URL,
+                             echo=True,
+                             pool_pre_ping=True)
 
 
-def get_session() -> Generator[Session, None, None]:
-    with Session(engine) as session:
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSession(engine) as session:
         yield session
 
 
-SessionDep = Annotated[Session, Depends(get_session)]
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
