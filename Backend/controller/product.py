338f823f8c4dffab5426446e5838eeeb873b188c
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2026-03-23 22:26:29 +0530

    now able to fetch product by id and update fetch all product

--- Backend/controller/product.py
+++ Backend/controller/product.py
@@ -1,9 +1,13 @@
+from uuid import UUID
+
 from fastapi import HTTPException
+from sqlalchemy.orm import selectinload
 from sqlmodel import select
 
-from model import SessionModel
+from model.session_model import SessionModel
 from model.database import SessionDep
 from model.product_model import ProductsModel
+from schemas.product_schema import ProductResponseModel
 
 
 async def fetch_all_product(db_session: SessionDep):
@@ -13,17 +17,26 @@ async def fetch_all_product(db_session: SessionDep):
 
     :return: Returns a list containing all products
     """
-    result = await db_session.exec(select(ProductsModel))
+    result = await db_session.exec(select(ProductsModel)
+                                   .options(selectinload(ProductsModel.seller)))
     all_product = result.all()
 
     if not len(all_product):
         raise HTTPException(status_code=404, detail="No product found")
 
-    return all_product
+    return [ProductResponseModel.model_validate(product) for product in all_product]
 
 
-def fetch_product_by_id():
-    return {"message": "Should return a specific product"}
+async def fetch_product_by_id(product_id: UUID, db_session: SessionDep) -> ProductResponseModel:
+    result = await db_session.exec(select(ProductsModel)
+                                   .where(ProductsModel.id == product_id)
+                                   .options(selectinload(ProductsModel.seller)))
+    product_in_db = result.first()
+
+    if not product_in_db:
+        raise HTTPException(status_code=404, detail="Product not found")
+
+    return ProductResponseModel.model_validate(product_in_db)
 
 
 def make_a_new_product(user: SessionModel, db_session: SessionDep):
