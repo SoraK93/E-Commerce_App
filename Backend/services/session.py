aba2afac43b58edf0ba14ab92af613dd46b7a8cf
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2026-03-23 09:58:18 +0530

    Add create, validate and delete for session_id This will handle all session related operations

--- Backend/services/session.py
+++ Backend/services/session.py
@@ -1,29 +1,74 @@
-from model.session_model import SessionModel, user_role
-from model.database import SessionDep, engine
+from datetime import datetime, timezone
+from typing import Annotated
+from uuid import UUID
+
+from fastapi import Cookie, Depends, HTTPException
 from pydantic import EmailStr
-from sqlmodel import delete, func, Session
+from sqlmodel import delete, func, select
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from model.database import SessionDep, engine
+from model.session_model import SessionModel, user_role
 
 
-def create_new_session(session: SessionDep, email: EmailStr, role: user_role):
+async def create_new_session(db_session: SessionDep, email: EmailStr, role: user_role):
     """Creates user session and stores it inside database
 
-    @param session: current active database session
-    @param email: user email received during login request
-    @param role: set user role ["admin", "seller", "customer"]
+    :param session: current active database session
+    :param email: user email received during login request
+    :param role: set user role ["admin", "seller", "customer"]
 
-    @return session"""
+    :returns session: return newly created session data
+    
+    :raise HTTPException: if session creation fails."""
     new_session = SessionModel(role=role, email=email)
 
-    session.add(new_session)
-    session.commit()
-    session.refresh(new_session)
+    try:
+        db_session.add(new_session)
+        await db_session.commit()
+        await db_session.refresh(new_session)
+    except Exception:
+        await db_session.rollback()
+        raise HTTPException(status_code=500, detail="Failed to create new session. Please try again later.")
 
     return new_session
 
 
-def delete_expired_session():
+async def validate_session(db_session: SessionDep, session_id: Annotated[UUID | None, Cookie()] = None):
+    """Verify session_id to check it has any available active session
+
+    :param session: current active database session
+    :param session_id: using UUID received from request.cookie
+
+    :returns SessionModel: If an active session is found  
+    :returns None: if no session_id was provided in the request  
+
+    :raise HTTPException: if session_id missing in database or expired"""
+    if not session_id:
+        return None
+
+    result = await db_session.exec(select(SessionModel)
+                                .where(SessionModel.id == session_id,
+                                       SessionModel.expire_at > datetime.now(timezone.utc)))
+    get_session_data = result.first()
+
+    if not get_session_data:
+        raise HTTPException(
+            status_code=401, detail="Invalid session. Please login again.")
+
+    return get_session_data
+
+
+valid_session_dep = Annotated[SessionModel, Depends(validate_session)]
+
+
+async def delete_expired_session():
     """ Handle deletion of expired session in database"""
-    with Session(engine) as session:
-        del_session = delete(SessionModel).where(SessionModel.expire_at < func.now())
-        session.exec(del_session)
-        session.commit()
+    async with AsyncSession(engine) as db_session:
+        del_session = delete(SessionModel).where(
+            SessionModel.expire_at < func.now())
+        try:
+            await db_session.exec(del_session)
+            await db_session.commit()
+        except Exception:
+            await db_session.rollback()
