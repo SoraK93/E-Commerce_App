1051dc0f1654e7ea1ca6a2365b865970ecea64ac
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2026-03-26 07:52:12 +0530

    seller can edit/ delete products, user can view product details

--- Backend/controller/product.py
+++ Backend/controller/product.py
@@ -1,13 +1,15 @@
+from datetime import timezone, datetime
 from uuid import UUID
 
-from fastapi import HTTPException
+from fastapi import HTTPException, status
 from sqlalchemy.orm import selectinload
 from sqlmodel import select
 
-from model.session_model import SessionModel
 from model.database import SessionDep
 from model.product_model import ProductsModel
-from schemas.product_schema import ProductResponseModel
+from model.session_model import SessionModel
+from model.user_model import UserModel
+from schemas.product_schema import ProductCreateModel, ProductResponseModel, ProductEditModel
 
 
 async def fetch_all_product(db_session: SessionDep):
@@ -18,34 +20,116 @@ async def fetch_all_product(db_session: SessionDep):
     :return: Returns a list containing all products
     """
     result = await db_session.exec(select(ProductsModel)
+                                   .where(ProductsModel.deleted_at == None)
                                    .options(selectinload(ProductsModel.seller)))
     all_product = result.all()
 
     if not len(all_product):
-        raise HTTPException(status_code=404, detail="No product found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No products loaded.")
 
     return [ProductResponseModel.model_validate(product) for product in all_product]
 
 
 async def fetch_product_by_id(product_id: UUID, db_session: SessionDep) -> ProductResponseModel:
+    """
+
+    :param product_id:
+    :param db_session:
+    :return:
+    """
+    result = await db_session.exec(select(ProductsModel)
+                                   .where(ProductsModel.id == product_id)
+                                   .options(selectinload(ProductsModel.seller)))
+    product_in_db = result.first()
+    if not product_in_db:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Unable to retrieve, product not found.")
+
+    return ProductResponseModel.model_validate(product_in_db)
+
+
+async def make_a_new_product(new_product: ProductCreateModel, db_session: SessionDep, user_session: SessionModel):
+    """
+
+    :param new_product:
+    :param db_session:
+    :param user_session:
+    :return:
+    """
+    result = await db_session.exec(select(UserModel)
+                                   .where(UserModel.email == user_session.email))
+    user_in_db: UserModel = result.first()
+    if not user_in_db:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Unable to create, user not found.")
+
+    product_data = ProductsModel(**new_product.model_dump(),
+                                 seller_id=user_in_db.id)
+
+    try:
+        db_session.add(product_data)
+        await db_session.commit()
+        await db_session.refresh(product_data)
+    except Exception:
+        await db_session.rollback()
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail="Failed to create new Product. Please try again later.")
+
+
+async def edit_existing_product_detail(product_id: UUID, edit_product: ProductEditModel,
+                                       user_session: SessionModel, db_session: SessionDep):
+    """
+
+    :param product_id:
+    :param edit_product:
+    :param user_session:
+    :param db_session:
+    :return:
+    """
     result = await db_session.exec(select(ProductsModel)
                                    .where(ProductsModel.id == product_id)
                                    .options(selectinload(ProductsModel.seller)))
     product_in_db = result.first()
 
+    # checks if product exists in database
     if not product_in_db:
-        raise HTTPException(status_code=404, detail="Product not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Unable to edit, product not found.")
 
-    return ProductResponseModel.model_validate(product_in_db)
+    # checks if seller and active user are same
+    if product_in_db.seller.email != user_session.email:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Not authorized to update this product.")
+
+    product_dict = edit_product.model_dump(exclude_unset=True)
+    product_in_db.sqlmodel_update(product_dict)
+
+    try:
+        db_session.add(product_in_db)
+        await db_session.commit()
+    except Exception:
+        await db_session.rollback()
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail="Failed to update product. Please try again later.")
 
 
-def make_a_new_product(user: SessionModel, db_session: SessionDep):
-    return {"message": "Should create a new product"}
+async def delete_existing_product(product_id: UUID, db_session: SessionDep):
+    """
 
+    :param product_id:
+    :param db_session:
+    :return:
+    """
+    result = await db_session.exec(select(ProductsModel).where(ProductsModel.id == product_id))
+    product_in_db: ProductsModel = result.first()
+    if not product_in_db:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Unable to delete, product not found.")
 
-def edit_existing_product_detail():
-    return {"message": "should update a single product"}
+    # getting current utc time of delete
+    current_utc_time = datetime.now(timezone.utc)
+    product_in_db.deleted_at = current_utc_time
 
-
-def delete_existing_product():
-    return {"message": "should delete a specific product"}
+    try:
+        db_session.add(product_in_db)
+        await db_session.commit()
+    except Exception:
+        await  db_session.rollback()
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail="Failed to delete product. Please try again later")
--- Backend/router/product.py
+++ Backend/router/product.py
@@ -1,9 +1,10 @@
 from uuid import UUID
 
-from fastapi import APIRouter
+from fastapi import APIRouter, status, HTTPException
 
 import controller.product as product
 from model.database import SessionDep
+from schemas.product_schema import ProductCreateModel, ProductEditModel, ProductResponseModel
 from services.session import valid_session_dep
 
 router = APIRouter(
@@ -12,7 +13,7 @@ router = APIRouter(
 )
 
 
-@router.get("/")
+@router.get("/", status_code=status.HTTP_200_OK, response_model=list[ProductResponseModel])
 async def get_products(db_session: SessionDep):
     """Retrieve all products
 
@@ -23,22 +24,70 @@ async def get_products(db_session: SessionDep):
     return await product.fetch_all_product(db_session)
 
 
-@router.get("/{product_id}")
+@router.get("/{product_id}", status_code=status.HTTP_201_CREATED, response_model=list[ProductResponseModel])
 async def get_product_by_id(product_id: UUID, db_session: SessionDep):
+    """
+
+    :param product_id:
+    :param db_session:
+    :return:
+    """
     product_in_db = await product.fetch_product_by_id(product_id, db_session)
+
     return [product_in_db]
 
 
-@router.post("/")
-async def create_new_product(user_session: valid_session_dep, db_session: SessionDep):
-    return product.make_a_new_product(user_session, db_session)
+@router.post("/", status_code=status.HTTP_204_NO_CONTENT)
+async def create_new_product(product_data: ProductCreateModel, db_session: SessionDep, user_session: valid_session_dep):
+    """
+
+    :param product_data:
+    :param db_session:
+    :param user_session:
+    :return:
+
+    :raise
+    """
+    if not user_session and user_session.role != "seller":
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Product creation not allowed. Please login and try again.")
+
+    await product.make_a_new_product(product_data, db_session, user_session)
 
 
-@router.patch("/{product_id}")
-async def update_product_by_id():
-    return product.edit_existing_product_detail()
+@router.patch("/{product_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_product_by_id(product_id: UUID, edit_product: ProductEditModel, user_session: valid_session_dep,
+                               db_session: SessionDep):
+    """
+
+    :param product_id:
+    :param edit_product:
+    :param user_session:
+    :param db_session:
+    :return:
+
+    :raise
+    """
+    if not user_session and user_session.role != "seller":
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Cannot update product details. Please login and try again.")
+
+    await product.edit_existing_product_detail(product_id, edit_product, user_session, db_session)
 
 
-@router.delete("/{product_id}")
-async def delete_product_by_id():
-    return product.delete_existing_product()
+@router.delete("/{product_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_product_by_id(product_id: UUID, user_session: valid_session_dep, db_session: SessionDep):
+    """
+
+    :param product_id:
+    :param user_session:
+    :param db_session:
+    :return:
+
+    :raise
+    """
+    if not user_session and user_session.role != "seller":
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Cannot delete product details. Please login and try again.")
+
+    await product.delete_existing_product(product_id, db_session)
