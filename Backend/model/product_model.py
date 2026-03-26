f4bcf7878bb9e73a82434d9c4121a687551e6cf9
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2026-03-26 07:46:18 +0530

    Adding deleted_at column to product table

--- Backend/model/product_model.py
+++ Backend/model/product_model.py
@@ -10,8 +10,7 @@ from sqlmodel import SQLModel, Field, Relationship
 class ProductsModel(SQLModel, table=True):
     __tablename__ = "products"
 
-    id: uuid.UUID = Field(default_factory=uuid.uuid4,
-                          primary_key=True, index=True, nullable=False, unique=True)
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
 
     name: str = Field(max_length=255, nullable=False)
 
@@ -20,13 +19,17 @@ class ProductsModel(SQLModel, table=True):
     in_stock: int = Field(default=0, ge=0, nullable=False)
 
     price: Decimal = Field(
-        default=0.00, decimal_places=2, ge=0, nullable=False)
+        default=Decimal("0.00"), decimal_places=2, ge=0, nullable=False)
+
+    deleted_at: datetime | None = Field(sa_column=Column(
+        DateTime(timezone=True), default=None, nullable=True, index=True))
 
     created_at: datetime = Field(sa_column=Column(
         DateTime(timezone=True), server_default=func.now(), nullable=True))
 
-    seller_id: Optional[uuid.UUID] = Field(
+    seller_id: uuid.UUID | None = Field(
         foreign_key="customers_details.id", ondelete="SET NULL", nullable=True)
 
     # will help in making seller related query without writing any extra query
     seller: Optional["UserModel"] = Relationship(back_populates="products")
+    cart: Optional["CartModel"] = Relationship(back_populates="product")
