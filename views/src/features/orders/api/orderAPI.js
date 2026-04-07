0a30f74225a4988ecca121fb5031dd63c6162079
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2026-04-07 10:43:45 +0530

    add create order api

--- views/src/features/orders/api/orderAPI.js
+++ views/src/features/orders/api/orderAPI.js
@@ -4,14 +4,34 @@ import { ENDPOINT } from "../../CONSTANT";
 // get list of order objects
 // use cart table to create order list
 
-export const getOrderList = createAsyncThunk("order/getOrderList", async () => {
-  const response = await fetch(`${ENDPOINT}/order`, {
+const getOrderList = createAsyncThunk("order/getOrderList", async () => {
+  const response = await fetch(`${ENDPOINT}/order/`, {
     method: "GET",
     credentials: "include",
   });
 
   const data = await response.json();
   if (!response.ok) throw new Error("Failed to fetch order list");
-  
+
   return data;
 });
+
+const createOrder = createAsyncThunk("order/createOrder", async (orderData) => {
+  console.log(orderData);
+  const response = await fetch(`${ENDPOINT}/order/`, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ cart: orderData }),
+  });
+
+  const data = await response.json();
+  if (!response.ok)
+    return { status: response.status, statusText: response.statusText };
+
+  return data;
+});
+
+export { getOrderList, createOrder };
